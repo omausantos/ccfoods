240d5eecd001c269f8846aa35341207cace77f4b
Mauricio Santos <mauwebjc@gmail.com>
2022-08-11 23:31:48 -0300

    [Debug] Remoção de Arquivos de Debug

--- Modulo01/Modulo01/Paginas/Entregadores/EntregadoresNewPage.xaml.cs
+++ Modulo01/Modulo01/Paginas/Entregadores/EntregadoresNewPage.xaml.cs
@@ -3,13 +3,44 @@ using System.Collections.Generic;
 
 using Xamarin.Forms;
 
+using Modulo01.Dal;
+using Modulo01.Modelo;
+
 namespace Modulo01.Paginas.Entregadores
 {
     public partial class EntregadoresNewPage : ContentPage
     {
+        private EntregadorDAL dalEntregador = EntregadorDAL.GetInstance();
+
         public EntregadoresNewPage()
         {
             InitializeComponent();
+            PrepararParaNovoEntregador();
+        }
+
+        private void PrepararParaNovoEntregador()
+        {
+            var novoId = dalEntregador.GetAll().Count + 1;
+            identregador.Text = novoId.ToString().Trim();
+            nome.Text = String.Empty;
+            telefone.Text = String.Empty;
+        }
+
+        public void BtnGravarClick(object sender, EventArgs args)
+        {
+            if(nome.Text.Trim() == String.Empty || telefone.Text.Trim() == String.Empty)
+            {
+                this.DisplayAlert("Erro", "Voce precisa informar o nome e telefone para o novo entregador", "ok");
+            } else
+            {
+                dalEntregador.Add(new Entregador()
+                {
+                    Id = Convert.ToInt32(identregador.Text),
+                    Nome = nome.Text,
+                    Telefone = telefone.Text
+                });
+                PrepararParaNovoEntregador();
+            }
         }
     }
 }
