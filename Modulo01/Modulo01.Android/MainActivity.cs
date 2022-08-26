8b3fbb1deb660b948de8b08cce59d338c6c025fc
Mauricio Santos <mauwebjc@gmail.com>
2022-08-25 22:21:14 -0300

    [Capitulo 05] ALTERANDO O ÍCONE, O NOME DA APLICAÇÃO E A COR DA PÁGINA DE ABERTURA

--- Modulo01/Modulo01.Android/MainActivity.cs
+++ Modulo01/Modulo01.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using Plugin.CurrentActivity;
 
 namespace Modulo01.Droid
 {
-    [Activity(Label = "Modulo01", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "CC Foods", Icon = "@drawable/cc2929", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
